833c98e81fa592c86ef85dd68977be38ecdd93db
banv <banv@ai-t.vn>
2019-02-27 14:53:44 +0700

    add log

--- server.js
+++ server.js
@@ -115,7 +115,50 @@ app.get('/authenticate', (req, res) => {
 })
 
 // Let's create our HTTPS server and we're ready to go.
-https.createServer(opts, app).listen(9999)
+// https.createServer(opts, app).listen(9999)
+var server =  https.createServer(opts, app);
+var port = 443;
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
+
 
 // Then we can start our server with `npm i && node server.js`.
 
